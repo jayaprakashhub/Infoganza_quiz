c11c79602f547cdaf8a5a815a751080058eea106
Jayaprakash <139214567+jayaprakashhub@users.noreply.github.com>
2025-04-28 20:49:20 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminResults from './AdminResults.jsx';
 import axios from 'axios';
 
-axios.defaults.baseURL="http://localhost:5001"
+axios.defaults.baseURL="https://infoganza-quiz.onrender.com"
 const App = () => {
   return (
     <Router>
